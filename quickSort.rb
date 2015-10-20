44b08cde78c168d8ab697970870ebdd90f807f3b
revC-Rain <ev50547@meiji.ac.jp>
2015-10-20 22:44:01 +0900

    Update quickSort.rb

--- quickSort.rb
+++ quickSort.rb
@@ -3,9 +3,9 @@ ary = Array.new(100){ Random.rand(100) }
 def quickSort(array)
 	return array if array.size <= 1 # 配列の大きさが0か1なら配列をそのまま返す
 
-	pivot = array[0]
-	arr_l = Array.new
-	arr_r = Array.new
+	pivot = array[0] # 軸を設定
+	arr_l = Array.new # 軸より左側の配列
+	arr_r = Array.new # 軸より右側の配列
 
 	for i in 1...array.size do
 		(array[i] > pivot ? arr_r : arr_l).push(array[i]) # 軸より大きければarr_rそうでなければarr_lに要素を追加
